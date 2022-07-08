5262b7edb94c8e9865654a761210f29cdf46625b
mlisztes <lisztesmoni@gmail.com>
2022-07-08 20:31:52 +0200

    refactor: create App component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 import './index.css';
-
-const App = () => {
-    return(
-        <div>
-            <h2>Hi!</h2>
-        </div>
-    );
-};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
