239a1df9a2560bff5ad4997b95543871a21098a7
mlisztes <lisztesmoni@gmail.com>
2025-03-29 15:52:19 +0100

    ci: fix GitHub Actions deployment script

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to reload with GitHub Actions.
+          Edit <code>src/App.jsx</code> and save to reload with GitHub Actions!
         </p>
       </div>
       <p className="read-the-docs">
