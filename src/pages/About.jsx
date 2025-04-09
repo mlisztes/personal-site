bd5e888b62c0ff2287107c2dfc24526ca87a6ff5
mlisztes <lisztesmoni@gmail.com>
2025-04-09 18:50:32 +0200

    feat: set up React Router and initial file structure

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+      <div>
+        <h1>Bemutatkozás</h1>
+        <p>...</p>
+      </div>
+    )
+  }
+  
+  export default About
+  
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,11 @@
+const Portfolio = () => {
+    return (
+      <div>
+        <h1>Versfordítások</h1>
+        <p>...</p>
+      </div>
+    )
+  }
+  
+  export default Portfolio
+  
