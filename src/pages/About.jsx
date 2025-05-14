5e25d80a530333717e130302248ae4aef586a96a
mlisztes <lisztesmoni@gmail.com>
2025-05-14 11:24:06 +0200

    chore: modify content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 const About = () => {
     return (
       <div className="content-container">
-        <p>Lisztes Éva (1989) magyar műfordító. Munkája elsősorban a 20. század első felének orosz irodalmával foglalkozik, különös figyelmet fordítva Marina Cvetajeva költészetére, valamint Vaszilij Kandinszkij dadaista műveire.</p>
+        <p>Lisztes Éva (1989) magyar műfordító. Munkája elsősorban a 20. század első felének orosz irodalmával foglalkozik, különös figyelmet fordítva Marina Cvetajeva költészetére.</p>
       </div>
     )
   }
